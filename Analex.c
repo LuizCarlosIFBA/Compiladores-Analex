6ca91de322789814890a4ab82afd96e4a50cd0ee
LuizCarlosIFBA <sacramento.tecnico@gmail.com>
2024-11-19 13:46:53 -0300

    ok

--- Analex.c
+++ Analex.c
@@ -8,12 +8,14 @@
 #define TAM_LEXEMA 50
 #define TAM_NUM 20
 
-void error(char msg[]) {
+void error(char msg[])
+{
     printf("%s\n", msg);
     exit(1);
 }
 
-TOKEN AnaLex(FILE *fd) {
+TOKEN AnaLex(FILE *fd)
+{
     int estado;
     char lexema[TAM_LEXEMA] = "";
     int tamL = 0;
@@ -22,148 +24,197 @@ TOKEN AnaLex(FILE *fd) {
 
     TOKEN t;
     estado = 0;
-    while (true) {
+    while (true)
+    {
         char c = fgetc(fd);
-        switch (estado) {
-            case 0:
-                if (c == ' ' || c == '\t') estado = 0;
-                else if (c >= 'a' && c <= 'z') { // inicio de identificador - inicializa lexema
-                    estado = 0;
-                    lexema[tamL] = c;
-                    lexema[++tamL] = '\0';
-                }else if (c == '"') {  // Início de uma string
-                        estado = 9;
-                        tamL = 0; // Reset para acumular string
-                }
-                else if (c >= '0' && c <= '9') { // inicio de constante inteira - inicializa digitos
-                    estado = 1;
-                    digitos[tamD] = c;
-                    digitos[++tamD] = '\0';
-                }
-                else if (c == '+') { // sinal de adicao - monta e devolve token
-                    estado = 28;
-                    t.cat = SN;
-                    t.codigo = ADICAO;
-                    return t;
-                }
-                else if (c == '-') { // sinal de subtracao - monta e devolve token
-                    estado = 27;
-                    t.cat = SN;
-                    t.codigo = SUBTRACAO;
-                    return t;
-                }
-                else if (c == '*') { // sinal de multiplicacao - monta e devolve token
-                    estado = 26;
-                    t.cat = SN;
-                    t.codigo = MULTIPLIC;
-                    return t;
-                }
-                else if (c == '/') { // sinal de divisao - monta e devolve token
-                    estado = 29;
-                    t.cat = SN;
-                    t.codigo = DIVISAO;
-                    return t;
-                }
-                else if (c == '=') { // sinal de atribuicao - monta e devolve token
-                    estado = 40;
-                    t.cat = SN;
-                    t.codigo = ATRIB;
-                    return t;
-                }
-                else if (c == '(') { // sinal de abre parenteses - monta e devolve token
-                    estado = 20;
-                    t.cat = SN;
-                    t.codigo = ABRE_PAR;
-                    return t;
-                }
-                else if (c == ')') { // sinal de fecha parenteses - monta e devolve token
-                    estado = 21;
-                    t.cat = SN;
-                    t.codigo = FECHA_PAR;
-                    return t;
-                }
-                else if (c == '\n') {
-                    estado = 0;
-                    t.cat = FIM_EXPR; // fim de linha (ou expressao) encontrado
-                    contLinha++;
-                    return t;
-                }
-                else if (c == EOF) { // fim do arquivo fonte encontrado
-                    t.cat = FIM_ARQ;
-                    return t;
-                }
-                else
-                    error("Caracter invalido na expressao!"); // sem transicao valida no AFD
-                break;
+        switch (estado)
+        {
+        case 0:
+            if (c == ' ' || c == '\t')
+                estado = 8;
+            else if (c >= 'a' && c <= 'z')
+            { // inicio de identificador - inicializa lexema
+                estado = 0;
+                lexema[tamL] = c;
+                lexema[++tamL] = '\0';
+            }
+            else if (c == '"')
+            { // Início de uma string
+                estado = 9;
+                tamL = 0; // Reset para acumular string
+            }
+            else if (c >= '0' && c <= '9')
+            { // inicio de constante inteira - inicializa digitos
+                estado = 1;
+                digitos[tamD] = c;
+                digitos[++tamD] = '\0';
+            }
+            else if (c == '+')
+            { // sinal de adicao - monta e devolve token
+                estado = 28;
+                t.cat = SN;
+                t.codigo = ADICAO;
+                return t;
+            }
+            else if (c == '-')
+            { // sinal de subtracao - monta e devolve token
+                estado = 27;
+                t.cat = SN;
+                t.codigo = SUBTRACAO;
+                return t;
+            }
+            else if (c == '*')
+            { // sinal de multiplicacao - monta e devolve token
+                estado = 26;
+                t.cat = SN;
+                t.codigo = MULTIPLIC;
+                return t;
+            }
+            else if (c == '/')
+            { // sinal de divisao - monta e devolve token
+                estado = 29;
+                t.cat = SN;
+                t.codigo = DIVISAO;
+                return t;
+            }
+            else if (c == '=')
+            { // sinal de atribuicao - monta e devolve token
+                estado = 40;
+                t.cat = SN;
+                t.codigo = ATRIB;
+                return t;
+            }
+            else if (c == '(')
+            { // sinal de abre parenteses - monta e devolve token
+                estado = 20;
+                t.cat = SN;
+                t.codigo = ABRE_PAR;
+                return t;
+            }
+            else if (c == ')')
+            { // sinal de fecha parenteses - monta e devolve token
+                estado = 21;
+                t.cat = SN;
+                t.codigo = FECHA_PAR;
+                return t;
+            }
+            else if (c == '\n')
+            {
+                estado = 0;
+                t.cat = FIM_EXPR; // fim de linha (ou expressao) encontrado
+                contLinha++;
+                return t;
+            }
+            else if (c == EOF)
+            { // fim do arquivo fonte encontrado
+                t.cat = FIM_ARQ;
+                return t;
+            }
+            else
+                error("Caracter invalido na expressao!"); // sem transicao valida no AFD
+            break;
 
-            case 111:
-                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || (c == '_')) {
-                    estado = 1;
-                    lexema[tamL] = c; // acumula caracteres lidos em lexema
-                    lexema[++tamL] = '\0';
-                }
-                else { // transicao OUTRO* do estado 1 do AFD
-                    estado = 2; // monta token identificador e retorna
-                    ungetc(c, fd);
-                    t.cat = ID;
-                    strcpy(t.lexema, lexema);
-                    return t;
-                }
-                break;
+        case 111:
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || (c == '_'))
+            {
+                estado = 1;
+                lexema[tamL] = c; // acumula caracteres lidos em lexema
+                lexema[++tamL] = '\0';
+            }
+            else
+            {               // transicao OUTRO* do estado 1 do AFD
+                estado = 2; // monta token identificador e retorna
+                ungetc(c, fd);
+                t.cat = ID;
+                strcpy(t.lexema, lexema);
+                return t;
+            }
+            break;
 
-            case 1:
-                if (c >= '0' && c <= '9') {
-                    estado = 2;
-                    digitos[tamD] = c; // acumula digitos lidos na variavel digitos
-                    digitos[++tamD] = '\0';
-                }
-                else if (c == '.') { // ponto decimal encontrado
-                    estado = 3; // vai para estado de número real
-                    digitos[tamD] = c;
-                    digitos[++tamD] = '\0';
-                }
-                else { // transicao OUTRO* do estado 10 do AFD
-                    estado = 1; // monta token constante inteira e retorna
-                    ungetc(c, fd);
-                    t.cat = CT_I;
-                    t.valInt = atoi(digitos);
-                    return t;
-                }
-                break;
+        case 1:
+            if (c >= '0' && c <= '9')
+            {
+                estado = 2;
+                digitos[tamD] = c; // acumula digitos lidos na variavel digitos
+                digitos[++tamD] = '\0';
+            }
+            else if (c == '.')
+            {               // ponto decimal encontrado
+                estado = 3; // vai para estado de número real
+                digitos[tamD] = c;
+                digitos[++tamD] = '\0';
+            }
+            else
+            {               // transicao OUTRO* do estado 10 do AFD
+                estado = 1; // monta token constante inteira e retorna
+                ungetc(c, fd);
+                t.cat = CT_I;
+                t.valInt = atoi(digitos);
+                return t;
+            }
+            break;
 
-            case 3: // estado de número real
-                if (c >= '0' && c <= '9') {
-                    digitos[tamD] = c; // acumula dígitos da parte fracionária
-                    digitos[++tamD] = '\0';
-                } else {
-                    estado = 6; // monta token constante real e retorna
-                    ungetc(c, fd);
-                    t.cat = CT_R;
-                    t.valReal = atof(digitos); // converte string para float
-                    return t;
-                }
-                break;
+        case 3: // estado de número real
+            if (c >= '0' && c <= '9')
+            {
+                digitos[tamD] = c; // acumula dígitos da parte fracionária
+                digitos[++tamD] = '\0';
+            }
+            else
+            {
+                estado = 6; // monta token constante real e retorna
+                ungetc(c, fd);
+                t.cat = CT_R;
+                t.valReal = atof(digitos); // converte string para float
+                return t;
+            }
+            break;
 
-            case 9:  // Estado para capturar conteúdo de uma string
-                if (c >= 32 && c <= 126 && c != '"') { // ASCII imprimível e diferente de aspas
-                    if (tamL < TAM_MAX_LEXEMA - 1) {   // Evita overflow
-                        lexema[tamL++] = c;
-                        lexema[tamL] = '\0';
-                    }
-                } else  if (c == '"') {  // Fim da string
-                            t.cat = CT_S;
-                            strcpy(t.valString, lexema);
-                            return t;
-                        } else {  // Caracter inválido em string
-                            error("Caracter inválido na string!");
-    }
-    break;
-    
+        case 9: // Estado para capturar conteúdo de uma string
+            if (c >= 32 && c <= 126 && c != '"')
+            { // ASCII imprimível e diferente de aspas
+                if (tamL < TAM_MAX_LEXEMA - 1)
+                { // Evita overflow
+                    lexema[tamL++] = c;
+                    lexema[tamL] = '\0';
+                }
+            }
+            else if (c == '"')
+            { // Fim da string
+                t.cat = CT_S;
+                strcpy(t.valString, lexema);
+                return t;
+            }
+            else
+            { // Caracter inválido em string
+                error("Caracter inválido na string!");
+            }
+            break;
+        case 10:
+            if (c >= 32 && c <= 126 && c != '"')
+            { // ASCII imprimível e diferente de aspas
+                if (tamL == 1)
+                {                        // Evita overflow
+                    lexema[tamL++] = c;  // Adiciona o char ao lexema
+                    lexema[tamL] = '\0'; // Finaliza o lexema com '\0'
+                }
+            }
+            else if (c == '"')
+            {                                // Fim da string (aspas encontradas)
+                t.cat = CT_C;                // Define a categoria como STRINGCON
+                strcpy(t.valChar, lexema); // Copia o conteúdo da string reconhecida
+                return t;                    // Retorna o token
+            }
+            else
+            {                                          // Caracter inválido em string
+                error("Caracter inválido no char!"); // Exibe mensagem de erro
+            }
         }
     }
 }
 
-int main() {
+int main()
+{
     FILE *fd;
     TOKEN tk;
 
@@ -172,58 +223,64 @@ int main() {
 
     printf("LINHA %d: ", contLinha);
 
-    while (1) { // laço infinito para processar a expressão até o fim de arquivo
+    while (1)
+    { // laço infinito para processar a expressão até o fim de arquivo
         tk = AnaLex(fd);
-        switch (tk.cat) {
-            case ID:
-                printf("<ID, %s> ", tk.lexema);
+        switch (tk.cat)
+        {
+        case ID:
+            printf("<ID, %s> ", tk.lexema);
+            break;
+        case SN:
+            switch (tk.codigo)
+            {
+            case ADICAO:
+                printf("<SN, ADICAO> ");
                 break;
-            case SN:
-                switch (tk.codigo) {
-                    case ADICAO:
-                        printf("<SN, ADICAO> ");
-                        break;
-                    case SUBTRACAO:
-                        printf("<SN, SUBTRACAO> ");
-                        break;
-                    case MULTIPLIC:
-                        printf("<SN, MULTIPLICACAO> ");
-                        break;
-                    case DIVISAO:
-                        printf("<SN, DIVISAO> ");
-                        break;
-                    case ATRIB:
-                        printf("<SN, ATRIBUICAO> ");
-                        break;
-                    case ABRE_PAR:
-                        printf("<SN, ABRE_PARENTESES> ");
-                        break;
-                    case FECHA_PAR:
-                        printf("<SN, FECHA_PARENTESES> ");
-                        break;
-                }
+            case SUBTRACAO:
+                printf("<SN, SUBTRACAO> ");
                 break;
-            case CT_I:
-                printf("<INTCON, %d> ", tk.valInt);
+            case MULTIPLIC:
+                printf("<SN, MULTIPLICACAO> ");
                 break;
-            case CT_R:
-                printf("<REALCON, %.2f> ", tk.valReal);
+            case DIVISAO:
+                printf("<SN, DIVISAO> ");
                 break;
-            case CT_S:
-                printf("<STRINGCON, \"%s\"> ", tk.valString);
+            case ATRIB:
+                printf("<SN, ATRIBUICAO> ");
                 break;
-            case FIM_EXPR:
-                printf("<FIM_EXPR, %d>\n", 0);
-                printf("LINHA %d: ", contLinha);
+            case ABRE_PAR:
+                printf("<SN, ABRE_PARENTESES> ");
                 break;
-            case FIM_ARQ:
-                printf(" <Fim do arquivo encontrado>\n");
+            case FECHA_PAR:
+                printf("<SN, FECHA_PARENTESES> ");
                 break;
+            }
+            break;
+        case CT_I:
+            printf("<INTCON, %d> ", tk.valInt);
+            break;
+        case CT_R:
+            printf("<REALCON, %.2f> ", tk.valReal);
+            break;
+        case CT_S:
+            printf("<STRINGCON, \"%s\"> ", tk.valString);
+            break;
+        case CT_C:
+            printf("<STRINGCON, \"%s\"> ", tk.valChar);
+            break;
+        case FIM_EXPR:
+            printf("<FIM_EXPR, %d>\n", 0);
+            printf("LINHA %d: ", contLinha);
+            break;
+        case FIM_ARQ:
+            printf(" <Fim do arquivo encontrado>\n");
+            break;
         }
-        if (tk.cat == FIM_ARQ) break;
+        if (tk.cat == FIM_ARQ)
+            break;
     }
 
     fclose(fd);
     return 0;
 }
-
--- Analex.h
+++ Analex.h
@@ -4,7 +4,7 @@
 #define TAM_MAX_LEXEMA 31
 
 // Cálculos aritméticos
-enum TOKEN_CAT { ID = 1, SN, CT_I, CT_R, CT_S, FIM_EXPR, FIM_ARQ };
+enum TOKEN_CAT { ID = 1, SN, CT_I, CT_R, CT_S, CT_C, FIM_EXPR, FIM_ARQ };
 
 /* Onde: ID: Identificador, SN: Sinal; CT_I: Constante numérica inteira, CT_R: Constante numérica real */
 enum SINAIS { ATRIB = 1, ADICAO, SUBTRACAO, MULTIPLIC, DIVISAO, ABRE_PAR, FECHA_PAR }; // Sinais válidos da linguagem
@@ -13,7 +13,8 @@ typedef struct {
     enum TOKEN_CAT cat; // deve receber uma das constantes de enum TOKEN_CAT
     union { // parte variável do registro
         int codigo; // para tokens das categorias SN
-        char valString[TAM_MAX_LEXEMA]; // valor de uma string para STRINGCON
+        char valString[TAM_MAX_LEXEMA]; // valor de um caractere STRINGCON
+        char valChar[TAM_MAX_LEXEMA]; // valor de um caractere sCHARCON
         char lexema[TAM_MAX_LEXEMA]; // cadeia de caracteres que corresponde ao nome do token da cat. ID
         int valInt; // valor da constante inteira em tokens da cat. CT_I
         float valReal; // valor da constante real em tokens da cat. CT_R
